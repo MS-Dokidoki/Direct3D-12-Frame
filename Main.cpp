a2dc51f4ebfb968027217ba4762a3462729cc4ca
绪山真寻 <MS_Dokidoki@outlook.com>
2023-03-26 15:12:18 +0800

    Direct3D 12 程序框架

---
+++ Main.cpp
@@ -0,0 +1,115 @@
+#include "D3DApp.h"
+#define USE_FRAMERESOURCE           // 默认开启帧资源
+using namespace Microsoft::WRL;
+using namespace D3DHelper;
+
+class D3DFrame : public D3DApp
+{
+public:
+    D3DFrame(HINSTANCE);
+    
+    virtual bool Initialize();
+
+protected:
+    virtual void Render(const GameTimer&);
+    virtual void Update(const GameTimer&);
+
+private:
+   
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    try
+    {
+        D3DFrame app(hInstance);
+
+        if(!app.Initialize())
+            return 0;
+        return app.Run();
+    }
+    catch(DXException& e)
+    {
+        MessageBox(NULL, e.ToString(), TEXT("HR Failed"), MB_OK);
+    }
+
+    return 0;
+}
+
+D3DFrame::D3DFrame(HINSTANCE hInstance) : D3DApp(hInstance)
+{
+
+}
+
+bool D3DFrame::Initialize()
+{
+#ifdef USE_FRAMERESOURCE
+    bUseFrameResource = 1;     
+#endif
+    if(!D3DApp::Initialize())
+        return 0;
+    
+    return 1;
+}
+
+void D3DFrame::Update(const GameTimer& t)
+{
+#ifdef USE_FRAMERESOURCE
+    // 在当前帧命令存放之前，先进行命令分配器的切换
+    iCurrFrameResourceIndex = (iCurrFrameResourceIndex + 1) % nFrameResourceCount;
+    pCurrFrameResource = &pFrameResources[iCurrFrameResourceIndex];
+    pCommandAllocator = pCurrFrameResource->pCommandAllocator;
+
+    // 若当前帧资源未执行完成，说明命令分配器全都被 GPU 使用，等待执行完成
+    if(pCurrFrameResource->iFence != 0 && pFence->GetCompletedValue() < pCurrFrameResource->iFence)
+    {
+        ThrowIfFailed(pFence->SetEventOnCompletion(pCurrFrameResource->iFence, hFenceEvent));
+        WaitForSingleObject(hFenceEvent, INFINITE);
+    }
+#endif
+}
+
+void D3DFrame::Render(const GameTimer& t)
+{
+    ThrowIfFailed(pCommandAllocator->Reset());
+    ThrowIfFailed(pCommandList->Reset(pCommandAllocator.Get(), NULL));
+    pCommandList->ResourceBarrier(
+    1, &CD3DX12_RESOURCE_BARRIER::Transition(
+        ppSwapChainBuffer[iCurrBackBuffer].Get(),
+        D3D12_RESOURCE_STATE_PRESENT,
+        D3D12_RESOURCE_STATE_RENDER_TARGET
+    ));
+
+    pCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), 1, &DepthStencilBufferView());
+    pCommandList->RSSetScissorRects(1, &ScissorRect);
+    pCommandList->RSSetViewports(1, &ScreenViewport);
+
+    float clearColor[] = {0.1f, 0.1f, 0.1f, 1.0f};
+    pCommandList->ClearRenderTargetView(CurrentBackBufferView(), clearColor, 0, NULL);
+    pCommandList->ClearDepthStencilView(DepthStencilBufferView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, NULL);
+
+//
+
+//
+
+    pCommandList->ResourceBarrier(
+    1, &CD3DX12_RESOURCE_BARRIER::Transition(
+        ppSwapChainBuffer[iCurrBackBuffer].Get(),
+        D3D12_RESOURCE_STATE_RENDER_TARGET,
+        D3D12_RESOURCE_STATE_PRESENT
+    ));
+    pCommandList->Close();
+
+	ID3D12CommandList* ppCommandLists[] = {pCommandList.Get()};
+	pCommandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+	
+    ThrowIfFailed(pSwapChain->Present(0, 0));
+    iCurrBackBuffer = (iCurrBackBuffer + 1) % nSwapChainBufferCount;
+	
+#ifdef USE_FRAMERESOURCE
+    pCurrFrameResource->iFence = (UINT)++iCurrentFence;
+    pCommandQueue->Signal(pFence.Get(), iCurrentFence);
+#else
+    FlushCommandQueue();
+#endif
+}
